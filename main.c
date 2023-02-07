a9e89d108f24a7ec405f06a2913fac5aae45d6a6
sandeepk991 <115530193+sandeepk991@users.noreply.github.com>
2023-02-07 21:01:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,30 +10,76 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<ctype.h>
-
-int main(int ac, char *av[])
+void	child_one(int *pipefd, int *fd, char **av, char **envp)
 {
-    int fd1;
-    int fd2;
-    char c;
+	char	**cmd;
+	pid_t	c_pid;
 
-    if (ac == 3)
-    {
-        fd1 = open(av[1], O_RDONLY);
-        fd2 = open(av[2], O_RDWR);
-        while (read(fd1, &c, 1) > 0)
-        {
-            write(fd2, &c, 1);
-            //printf("number of lines : %i", )
-        }
-        close(fd1);
-        close(fd2);
-    }
-    else
-        printf("error");
-    return (0);
+	c_pid = fork();
+	if (c_pid < 0)
+		terminate("fork");
+	if (c_pid == 0)
+	{
+		fd[0] = open(av[1], O_RDONLY);
+		if (fd[0] < 0)
+			terminate(av[1]);
+		cmd = ft_split(av[2], ' ');
+		close(pipefd[0]);
+		dup2(fd[0], STDIN_FILENO);
+		dup2(pipefd[1], STDOUT_FILENO);
+		if (cmd[0] && path(cmd[0], envp))
+		{
+			execve(path(cmd[0], envp), cmd, envp);
+			free_split(cmd);
+		}
+		else
+			cmd_not_found(cmd);
+	}
+}
+
+void	child_two(int *pipefd, int *fd, char **av, char **envp)
+{
+	char	**cmd;
+	pid_t	c_pid;
+
+	c_pid = fork();
+	if (c_pid < 0)
+		terminate("fork");
+	if (c_pid == 0)
+	{
+		fd[1] = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd[1] < 0)
+			terminate(av[4]);
+		cmd = ft_split(av[3], ' ');
+		close(pipefd[1]);
+		dup2(fd[1], STDOUT_FILENO);
+		dup2(pipefd[0], STDIN_FILENO);
+		if (cmd[0] && path(cmd[0], envp))
+		{
+			execve(path(cmd[0], envp), cmd, envp);
+			free_split(cmd);
+		}
+		else
+			cmd_not_found(cmd);
+	}
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	int	pipefd[2];
+	int	fd[2];
+	int	status;
+
+	errno = 0;
+	if (ac != 5)
+		usage();
+	if (pipe(pipefd) < 0)
+		terminate(NULL);
+	child_one(pipefd, fd, av, envp);
+	child_two(pipefd, fd, av, envp);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(-1, &status, 0);
+	waitpid(-1, &status, 0);
+	return (0);
 }
